071f6c1283faad5b42c30bece5a7753685acb6c2
unknown <20130376@st.hcmuaf.edu.vn>
2023-12-29 18:56:45 +0700

    update group message api

---
+++ api/group_messages/getMember.php
@@ -0,0 +1,36 @@
+<?php
+include_once '../../lib/DatabaseConnection.php';
+
+function getMember( $groupId) {
+    $db = new DatabaseConnection();
+    $conn = $db->connect();
+
+
+    $stmt = $conn->prepare("SELECT u.user_id, u.username, u.profile_image_id, gm.role
+    FROM users u
+    JOIN group_members gm ON u.user_id = gm.user_id
+    WHERE gm.group_id = ?;");
+
+    $stmt->bind_param("i",  $groupId);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $friends = array();
+    while ($row = $result->fetch_assoc()) {
+        array_push($friends, $row);
+    }
+
+    $stmt->close();
+    $db->close();
+
+    echo json_encode(["success" => true, "friends" => $friends]);
+}
+
+header('Content-Type: application/json; charset=utf-8');
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $groupId = isset($_GET['groupId']) ? (int) $_GET['groupId'] : 0;
+
+    getMember($groupId);
+}
+?>
